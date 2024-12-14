7d9004785d2e242e99123d12542f75e0d28e4376
surajshinde272 <142134249+surajshinde272@users.noreply.github.com>
2024-12-14 14:53:19 +0530

    change

--- src/main/java/com/software/DatabaseConnection.java
+++ src/main/java/com/software/DatabaseConnection.java
@@ -11,7 +11,7 @@ public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/petmanagement";
 
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "root";
+    private static final String PASSWORD = "Suraj@2728";
 
     public static Connection getConnection() throws SQLException {
     	        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
